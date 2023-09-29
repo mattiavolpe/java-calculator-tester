82ba194182f53065c0e78e8a501cae45747c7cd1
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-09-29 12:32:17 +0200

    FIRST commit with Calculator class

---
+++ src/main/java/org/java/test/Calculator.java
@@ -0,0 +1,19 @@
+package org.java.test;
+
+public class Calculator {
+	public static float add (float num1, float num2) {
+		return num1 + num2;
+	}
+	
+	public static float subtract (float num1, float num2) {
+		return num1 - num2;
+	}
+	
+	public static float divide (float num1, float num2) {
+		return num1 / num2;
+	}
+	
+	public static float multiply (float num1, float num2) {
+		return num1 * num2;
+	}
+}
---
+++ src/main/java/org/java/test/Main.java
@@ -0,0 +1,9 @@
+package org.java.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
